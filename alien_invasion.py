ae118a58c1e4309cf02a5fa5bcb8431e4b6d3a3c
Erica Gillis <erica_gillis1@baylor.edu>
2021-02-24 17:47:21 -0600

    first  class commit

---
+++ alien_invasion.py
@@ -0,0 +1,26 @@
+import sys
+import pygame
+
+
+class AlienInvasion:
+    def __init__(self):
+        # initialize game
+        pygame.init()
+        # display window
+        self.screen = pygame.display.set_mode((1200, 800))
+        pygame.display.set_caption("Alien Invasion")
+
+    def run_game(self):
+        # start main loop
+        while True:
+            # watch for keyboard and mouse events
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+            # make most recently drawn screen visible
+            pygame.display.flip()
+
+    if __name__ == "__main__":
+        # make game and run
+        ai = AlienInvasion()
+        ai.run_game()
