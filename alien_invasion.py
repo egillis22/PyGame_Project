f12efcb3cb09970aedf82c824bebb7fc63c8fc4b
Erica Gillis <erica_gillis1@baylor.edu>
2021-02-24 19:40:15 -0600

    contiuous right movement

--- alien_invasion.py
+++ alien_invasion.py
@@ -21,6 +21,7 @@ class AlienInvasion:
         # start main loop
         while True:
             self._check_events()
+            self.ship.update()
             self._update_screen()
 
     def _check_events(self):
@@ -30,7 +31,10 @@ class AlienInvasion:
                 sys.exit()
             elif event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_RIGHT:
-                    self.ship.rect.x += 1
+                    self.ship.moving_right = True
+            elif event.type == pygame.KEYUP:
+                if event.key == pygame.K_RIGHT:
+                    self.ship.moving_right = False
 
     def _update_screen(self):
         self.screen.fill(self.settings.bg_color)
--- ship.py
+++ ship.py
@@ -6,12 +6,16 @@ class Ship:
         # initialize ship and start position
         self.screen = ai_game.screen
         self.screen_rect = ai_game.screen.get_rect()
-
+        self.moving_right = False
         # load ship image and get its rect
         self.image = pygame.image.load("images/ship.bmp")
         self.rect = self.image.get_rect()
         # start each ship at bottom center
         self.rect.midbottom = self.screen_rect.midbottom
+
+    def update(self):
+        if self.moving_right:
+            self.rect.x += 1
 
     def blitme(self):
         # draw ship at current location
