e53c3430aa7df88c5aa73d99c0005fb308afb3f1
Erica Gillis <erica_gillis1@baylor.edu>
2021-02-24 21:09:07 -0600

    quit button and fullscreen

--- alien_invasion.py
+++ alien_invasion.py
@@ -10,9 +10,10 @@ class AlienInvasion:
         pygame.init()
         self.settings = Settings()
         # display window
-        self.screen = pygame.display.set_mode(
-            (self.settings.screen_width, self.settings.screen_height)
-        )
+        self.screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+        self.settings.screen_width = self.screen.get_rect().width
+        self.settings.screen_height = self.screen.get_rect().height
+
         pygame.display.set_caption("Alien Invasion")
         # background
         self.ship = Ship(self)
@@ -39,6 +40,8 @@ class AlienInvasion:
             self.ship.moving_right = True
         elif event.key == pygame.K_LEFT:
             self.ship.moving_left = True
+        elif event.key == pygame.K_q:
+            sys.exit()
 
     def _check_keyup_events(self, event):
         if event.key == pygame.K_RIGHT:
