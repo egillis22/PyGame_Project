a43a36d9039d5f46beda6f4175c6a654a32c206c
Erica Gillis <erica_gillis1@baylor.edu>
2021-02-24 19:50:15 -0600

    changed ship speed

--- ship.py
+++ ship.py
@@ -5,6 +5,7 @@ class Ship:
     def __init__(self, ai_game):
         # initialize ship and start position
         self.screen = ai_game.screen
+        self.settings = ai_game.settings
         self.screen_rect = ai_game.screen.get_rect()
         self.moving_right = False
         self.moving_left = False
@@ -13,12 +14,14 @@ class Ship:
         self.rect = self.image.get_rect()
         # start each ship at bottom center
         self.rect.midbottom = self.screen_rect.midbottom
+        self.x = float(self.rect.x)
 
     def update(self):
         if self.moving_right:
-            self.rect.x += 1
+            self.x += self.settings.ship_speed
         if self.moving_left:
-            self.rect.x -= 1
+            self.x -= self.settings.ship_speed
+        self.rect.x = self.x
 
     def blitme(self):
         # draw ship at current location
